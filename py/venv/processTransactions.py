ea15c964521d0b843175acc1befee87ddefa4c96
RRuiz <rruiz@xn--razalog-2rb.com>
2024-07-19 02:01:45 -0300

    Tested with retrys and exception handling

--- py/venv/processTransactions.py
+++ py/venv/processTransactions.py
@@ -9,7 +9,8 @@ import logging
 import time
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(filename='invenda.log', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s: %(message)s ',filename='invenda.log', level=logging.INFO)
+
 today = datetime.today()
 today_str= today.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -37,8 +38,8 @@ except IndexError:
 date1 = datetime.strptime(date_cad1, '%Y-%m-%dT%H:%M:%S')
 date2 = datetime.strptime(date_cad2, '%Y-%m-%dT%H:%M:%S')
 wso2_url ='http://192.168.0.176:8290' #'http://34.130.249.21:8290' 
-c_retrys = 5 #amount of retrys
-c_sleeptime = 300
+c_retrys = 3 #amount of retrys
+c_sleeptime = 25 #300
 
 # amount of days to loop
 days= (date2 - date1).days
@@ -57,31 +58,68 @@ date_to = date1
 for day in range(0,days):
     date_to = date_to + timedelta(days=1)
     date_to_str = date_to.isoformat()
-
+    sys.stdout.write(".")
     logger.info(f"processsing from {date_from_str} to {date_to_str} from page {from_page}" )
     logger.info("======================================================================================================")
+
     #build wso2 api url and get first page for this date
     invenda_api_url = f"{wso2_url}/invenda/consume/transactions?dateFrom={date_from_str}.000Z&dateTo={date_to_str}.000Z&page="
     logger.info(f"requesting url {invenda_api_url}{from_page}")
-    response =requests.get(invenda_api_url+str(from_page)).json()
-    logger.info(response)
- 
+    try:
+        response_raw =requests.get(invenda_api_url+str(from_page))
+        try: logger.info(response_raw.json()) 
+        except Exception: logger.info(response_raw)        
+        response_raw.raise_for_status()
+    except requests.HTTPError as e:
+        logger.info(f"Error {e}")
+
+        for counter in range(1,int(c_retrys+1)):
+            logger.info(f"There was an error {response_raw}, sleeping for {str(c_sleeptime)} seconds...")
+            time.sleep(c_sleeptime)
+            logger.info(f"Retrying page {from_page}, try # {str(counter)}")
+            try:
+                response_raw =requests.get(invenda_api_url+f"{from_page}")
+                try: logger.info(response_raw.json()) 
+                except Exception: logger.info(response_raw) 
+                response_raw.raise_for_status()
+                break
+            except requests.HTTPError as e:
+                logger.info(f"Error {e}")
+                if counter == c_retrys: # last loop, raise Exception and stop processing
+                    logger.info("ABORTING: Maximum retry limit reached")
+                    raise Exception("ABORTING: Seem that service is down, please check the log file")
+
+    response = response_raw.json()
     totalPages = response["totalPages"]
     logger.info(f"total pages to consume {totalPages}")
 
     # get from page 2 to the last page for this date, this will call Invenda api and later insert into database
     for page in range(from_page+1,int(totalPages)):
         logger.info(f"About to request page {page}")
-        response =requests.get(invenda_api_url+f"{page}").json()
-        logger.debug(response)
-        if "invenda_errors" in response:
-            for counter in range(1,int(c_retrys)
-                logger.error(f"There was an error {response}, sleeping for {c_sleeptime} seconds...")
-                 time.sleep(c_sleeptime)
-                 logger.info(f"Retrying page {page}, try # {counter}")
-                 response =requests.get(invenda_api_url+f"{page}").json()
-                logger.debug(response)
+        try:
+            response_raw =requests.get(invenda_api_url+f"{page}")
+            try: logger.info(response_raw.json()) 
+            except Exception: logger.info(response_raw)
+            response_raw.raise_for_status()
+        except requests.HTTPError as e:
+            logger.info(f"Error {e}")
 
+            for counter in range(1,int(c_retrys+1)):
+                logger.info(f"There was an error {response_raw}, sleeping for {str(c_sleeptime)} seconds...")
+                time.sleep(c_sleeptime)
+                logger.info(f"Retrying page {str(page)}, try # {str(counter)}")
+                try:
+                    response_raw =requests.get(invenda_api_url+f"{page}")
+                    try: logger.info(response_raw.json()) 
+                    except Exception: logger.info(response_raw)
+                    response_raw.raise_for_status()
+                    break
+                except requests.HTTPError as e:
+                    logger.info(f"Error {e}")
+                if counter == c_retrys: # last loop, raise Exception and stop processing
+                    logger.info("ABORTING: Maximum retry limit reached")
+                    raise Exception("ABORTING: Seem that service is down, please check the log file")
+        sys.stdout.write(".")
 
     #reset from page counter for next day, in case from_page parameter was informed for first day in range
     from_page = 1
